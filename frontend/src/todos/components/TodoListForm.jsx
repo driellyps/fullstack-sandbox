04023f6a0fff3ab77e718e9e63e8ac9160036a9d
Drielly Hejdenberg <driellys@live.com>
2022-06-14 08:59:18 +0200

    add checkbox to mark task as completed and auto-save functionality

--- frontend/src/todos/components/TodoListForm.jsx
+++ frontend/src/todos/components/TodoListForm.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from 'react'
-import { TextField, Card, CardContent, CardActions, Button, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { TextField, Card, CardContent, CardActions, Button, Typography, Checkbox } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import AddIcon from '@mui/icons-material/Add'
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
 export const TodoListForm = ({ todoList, saveTodoList }) => {
   const [todos, setTodos] = useState(todoList.todos)
+  const [isSaving, setIsSaving] = useState(false)
+
+  useEffect(() => {
+   saveUpdatedList()
+  }, [todos])
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    saveTodoList(todoList.id, { todos })
+    saveUpdatedList()
+  }
+
+  const saveUpdatedList = () => {
+    sleep(1000).then(() => {
+      saveTodoList(todoList.id, todos)
+      setIsSaving(false)
+    })
   }
 
   return (
@@ -16,23 +30,48 @@ export const TodoListForm = ({ todoList, saveTodoList }) => {
       <CardContent>
         <Typography component='h2'>{todoList.title}</Typography>
         <form
-          onSubmit={handleSubmit}
+          onSubmit={(event) => {
+            setIsSaving(true)
+            handleSubmit(event)
+          }}
           style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}
         >
-          {todos.map((name, index) => (
+          {todos.map((todo, index) => (
             <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
               <Typography sx={{ margin: '8px' }} variant='h6'>
                 {index + 1}
-              </Typography>
+              <Checkbox
+                color='success'
+                checked={todo.completed}
+                title='Is task finished?'
+                onChange={() => {
+                  setIsSaving(true)
+                  setTodos([ // immutable update
+                    ...todos.slice(0, index),
+                    { task: todo.task, completed: !todo.completed, dueDate: todo.dueDate },
+                    ...todos.slice(index + 1)
+                  ])
+                  
+                }}
+              />          
+              </Typography> 
               <TextField
-                sx={{ flexGrow: 1, marginTop: '1rem' }}
+                sx={{ 
+                  flexGrow: 1, 
+                  marginTop: '1rem', 
+                  textDecoration: todo.completed && 'line-through #5C985C', 
+                  background: todo.completed && '#5C985C15',
+                  transition: 'background 0.3s'
+                }}
+                disabled={todo.completed}
                 label='What to do?'
-                value={name}
+                value={todo.task}
                 onChange={(event) => {
+                  setIsSaving(true)
                   setTodos([
                     // immutable update
                     ...todos.slice(0, index),
-                    event.target.value,
+                    { task: event.target.value, completed: todo.completed, dueDate: todo.dueDate },
                     ...todos.slice(index + 1),
                   ])
                 }}
@@ -42,6 +81,7 @@ export const TodoListForm = ({ todoList, saveTodoList }) => {
                 size='small'
                 color='secondary'
                 onClick={() => {
+                  setIsSaving(true)
                   setTodos([
                     // immutable delete
                     ...todos.slice(0, index),
@@ -58,13 +98,13 @@ export const TodoListForm = ({ todoList, saveTodoList }) => {
               type='button'
               color='primary'
               onClick={() => {
-                setTodos([...todos, ''])
+                setTodos([...todos, { task: '', completed: false, dueDate: '' }])
               }}
             >
               Add Todo <AddIcon />
             </Button>
-            <Button type='submit' variant='contained' color='primary'>
-              Save
+            <Button type='submit' variant='contained' color='primary' disabled={isSaving}>
+              {isSaving ? 'Saving...' : 'Save'}
             </Button>
           </CardActions>
         </form>
